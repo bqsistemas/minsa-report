51f8bfd750ecdf3e7b124b6a2d53b8f9044e9b57
jhon barrantes <jbarrantes>
2022-10-01 20:24:43 -0500

    feat(frontend): clear form and reset establishment

--- frontend/src/app/core/services/auth/auth.service.ts
+++ frontend/src/app/core/services/auth/auth.service.ts
@@ -38,6 +38,7 @@ export class AuthService {
       if (data.auth_token) {
         // this.setUser(data.data.user);
         localStorage.setItem(environment.codeJwt, data.auth_token);
+        console.log('EPALE')
         const permisos: any = await this.httpAjaxService.getWithOutPromise(`${environment.apis.apiSecurity}/permisos/`).toPromise()
         const appName = permisos?.authorization?.auth_apps[environment.appName]
 
@@ -66,6 +67,7 @@ export class AuthService {
     location.href = "/tablero-vih/";
   }
   setPermisos() {
+    console.log('EPALE 2')
     return this.httpAjaxService.getWithOutPromise(`${environment.apis.apiSecurity}/permisos/`)
   }
 
