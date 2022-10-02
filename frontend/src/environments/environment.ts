9cf2a72900bfb35b6b774aefef7951ed4c29b74e
jhon barrantes <jbarrantes>
2022-10-01 20:32:29 -0500

    feat(frontend): clear form and reset establishment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   codeTokenRolSede: '_jbq|minsa|rs',
   appName: 'pe.gob.minsa.vih',
   module: 'EVIH0',
-  role: '121',
+  role: '1212',
   apis: {
     backend: 'https://localhost:5001/api',
     apiLogin: 'https://dsihce.minsa.gob.pe/login/auth/login/',
