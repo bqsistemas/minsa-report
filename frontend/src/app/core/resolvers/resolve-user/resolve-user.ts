3052813a41e1268a429d9ecd620103da10347c8b
jhon barrantes <jbarrantes>
2022-10-01 20:21:52 -0500

    feat(frontend): clear form and reset establishment

--- frontend/src/app/core/resolvers/resolve-user/resolve-user.ts
+++ frontend/src/app/core/resolvers/resolve-user/resolve-user.ts
@@ -18,39 +18,45 @@ export class ResolveUser implements Resolve<any> {
     ) { }
 
     resolve(route: ActivatedRouteSnapshot) {
-        return this._authService.setPermisos().pipe(
-            map(async (response: any) => {
-                console.log('response', response)
-                const appName = response?.authorization?.auth_apps[environment.appName]
-
-                let hasRole121 = false
-                for(const k in response?.authorization?.permissions){
-                    const objectAppPermissions = response?.authorization?.permissions[k][environment.appName]
-                    if(objectAppPermissions 
-                        && objectAppPermissions[environment.module] 
-                        && objectAppPermissions[environment.module].indexOf(environment.role) >= 0) hasRole121 = true
-                }
-                if(!appName || !hasRole121){
+        try{
+            return this._authService.setPermisos().pipe(
+                map(async (response: any) => {
+                    console.log('response', response)
+                    const appName = response?.authorization?.auth_apps[environment.appName]
+    
+                    let hasRole121 = false
+                    for(const k in response?.authorization?.permissions){
+                        const objectAppPermissions = response?.authorization?.permissions[k][environment.appName]
+                        if(objectAppPermissions 
+                            && objectAppPermissions[environment.module] 
+                            && objectAppPermissions[environment.module].indexOf(environment.role) >= 0) hasRole121 = true
+                    }
+                    if(!appName || !hasRole121){
+                        localStorage.removeItem(environment.codeJwt);
+                        location.reload()
+                    }
+    
+                    this._authService.setUser({
+                        apellidoMaterno: response.lastname_mother,
+                        apellidoPaterno: response.lastname_father,
+                        nombres: response.name,
+                        numeroDocumento: response.document_number,
+                        tipoDocumento: response.document_type,
+                        userName: response.username,
+                        diresa: response.authorization.diresa,
+                        permissions: response?.authorization?.permissions
+                      } as User);
+                    return response;
+                }, catchError((err, caught) => {
+                    console.log('catch', err)
                     localStorage.removeItem(environment.codeJwt);
-                    location.reload()
-                }
-
-                this._authService.setUser({
-                    apellidoMaterno: response.lastname_mother,
-                    apellidoPaterno: response.lastname_father,
-                    nombres: response.name,
-                    numeroDocumento: response.document_number,
-                    tipoDocumento: response.document_type,
-                    userName: response.username,
-                    diresa: response.authorization.diresa,
-                    permissions: response?.authorization?.permissions
-                  } as User);
-                return response;
-            }, catchError((err, caught) => {
-                console.log('catch', err)
-                localStorage.removeItem(environment.codeJwt);
-                return null  
-            }))
-        ).toPromise();
+                    return null  
+                }))
+            ).toPromise(); 
+        }catch(err){
+            console.log('error', err)
+            localStorage.removeItem(environment.codeJwt);
+            return null  
+        }
     }
 }
